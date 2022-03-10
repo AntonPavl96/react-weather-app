f43ffaacab39ab385ab72f3f148c739c4bf4311d
Anton Pavl <antonpavl96@gmail.com>
2022-03-10 15:10:21 +0200

    added base api connection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+const api = {
+  key: "a385157eb5d2d1a2e91b7499206ea097",
+  base: "https://api.openweathermap.ord/data/2.5/",
+};
+
 const App = () => {
   return <div>App</div>;
 };
